9def7d0680c790546246d9d1755df7e8a90bdc10
Stanislav Kalashnik <s.kalashnik@infomir.com>
2018-06-08 15:27:20 +0300

    move code from spasdk/tasks repo and rework

---
+++ index.js
@@ -0,0 +1,66 @@
+/**
+ * @author Stanislav Kalashnik <darkpark.main@gmail.com>
+ * @license GNU GENERAL PUBLIC LICENSE Version 3
+ */
+
+'use strict';
+
+var name  = 'pug',
+    tools = require('@runner/tools'),
+    log   = require('@runner/logger').wrap(name);
+
+
+function build ( config, done ) {
+    var pug = require('pug'),
+        render, data;
+
+    try {
+        // prepare function and data
+        render = pug.compileFile(config.source, config.options || {});
+        data   = render(config.variables || {});
+
+        // save generated result
+        tools.write([{name: config.target, data: data}], log, done);
+    } catch ( error ) {
+        log.fail(error.toString());
+    }
+}
+
+
+function generator ( config, options ) {
+    var tasks = {};
+
+    // sanitize and extend defaults
+    options = Object.assign(generator.options, options || {});
+
+    tasks[options.prefix + 'config' + options.suffix] = function () {
+        log.inspect(config, log);
+    };
+
+    tasks[options.prefix + 'build' + options.suffix] = function ( done ) {
+        build(config, done);
+    };
+
+    tasks[options.prefix + 'clear' + options.suffix] = function ( done ) {
+        tools.unlink([config.target], log, done);
+    };
+
+    return tasks;
+}
+
+
+// defaults
+generator.options = {
+    prefix: name + ':',
+    suffix: ''
+};
+
+
+// export main actions
+generator.methods = {
+    build: build
+};
+
+
+// public
+module.exports = generator;
